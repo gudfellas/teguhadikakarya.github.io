6542e6488b8611c2cfaeeeb24dce5b683bd48e6c
gudfellas <linardo.simanjuntak@gmail.com>
2020-05-05 18:30:54 +0700

    first commit

---
+++ js/main.js
@@ -0,0 +1,35 @@
+
+
+function pageTransition() {
+
+    var tl = gsap.timeline();
+
+    tl.to('ul.transition li', { duration: .5, scaleY: 1, transformOrigin: "bottom left", stagger: .2 })
+    tl.to('ul.transition li', { duration: .5, scaleY: 0, transformOrigin: "bottom left", stagger: .1, delay: .1})
+
+}
+
+function delay(n) {
+    n = n || 2000;
+    return new Promise(done => {
+        setTimeout(() => {
+            done();
+        }, n);
+    });
+}
+
+barba.init({
+    sync: true,
+    transitions: [{
+      name: 'opacity-transition',
+      async leave(data) {
+        
+        const done = this.async();
+        pageTransition();
+        await delay(1390);
+        done();
+      },
+      enter(data) {
+      }
+    }]
+  });
